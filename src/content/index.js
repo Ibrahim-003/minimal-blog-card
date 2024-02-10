ef4a01f2164ccbd4a6238a0043d65fa34ef77554
Waldemar Almeyda <argenisibrainwaldemar@gmail.com>
2024-02-10 15:28:11 -0500

    updating images

--- src/content/index.js
+++ src/content/index.js
@@ -1,5 +1,5 @@
 export const cardContent = {
-  url: "./src/assets/cactus_img.jpg",
+  url: "./cactus_img.jpg",
   title: "Embracing Minimalism",
   tag: "Design",
   body: "From minimalist sculptures to minimalist paintings, this blog will inspire you to appreciate the beauty that lies in simplicity.",
