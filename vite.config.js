de5ea2e49b950ab59c94a31eb1da36a8ebe388fd
Waldemar Almeyda <argenisibrainwaldemar@gmail.com>
2024-02-10 15:57:39 -0500

    updating github-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/minimal-blog-card/",
+  base: "https://Ibrahim-003.github.io/minimal-blog-card/",
 });
